b351655384841e7ca473d1ab3d1f6e4635976c7d
bonvi83 <bonvi83@gmail.com>
2024-01-11 16:55:51 +0100

    add macro struttura html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
+
+
 // CODICE DI BASE
-// for (var i = 1; i <= 100; i++) {
-//     if (i % 3 == 0 && i % 5 == 0) {
-//         console.log('FizzBuzz');
-//     } else if (i % 3 == 0) {
-//         console.log('Fizz');
-//     } else if (i % 5 == 0) {
-//         console.log('Buzz');
-//     } else {
-//         console.log(i);
-//     }
-// }
+for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+
+    } else if (i % 3 == 0) {
+        console.log('Fizz');
+
+    } else if (i % 5 == 0) {
+        console.log('Buzz');
+
+    } else {
+        console.log(i);
+    }
+}
+
+
+
+// cardRow.innerHTML += `
+//     <div class="col-3">
+//         <div class="card ${classes}">
+//             <h3>${result}</h3>
+//         </div>
+//     </div>
+//     `
