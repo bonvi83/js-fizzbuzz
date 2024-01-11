499a72c0b6efc46cbb55bb8eb8fd4ac6b9bba8eb
bonvi83 <bonvi83@gmail.com>
2024-01-11 18:07:35 +0100

    per me esercizxio finisce qui

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
-let rowElement = document.querySelector(".row");
+// COMANDO DI MATTIA PER VISUALIZZARE TUTTE LE CARD
+// let rowElement = document.querySelector(".row");
+
+// let card = document.getElementById("output-nome").innerHTML;
+
 
 // CODICE DI BASE
 for (var i = 1; i <= 100; i++) {
+    let result; 
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
+        result = "FizzBuzz";
+        // console.log('FizzBuzz');
 
     } else if (i % 3 == 0) {
-        console.log('Fizz');
+        result = "Fizz";
+        // console.log('Fizz');
 
     } else if (i % 5 == 0) {
-        console.log('Buzz');
+        result = "Buzz";
+        // console.log('Buzz');
 
     } else {
-        console.log(i);
+        result = i;
+        // console.log(i);
     }
     rowElement.innerHTML += "<div class='card'>00</div>";
 }
 
 
-console.log(rowElement)
-// cardRow.innerHTML += `
-//     <div class="col-3">
-//         <div class="card ${classes}">
-//             <h3>${result}</h3>
-//         </div>
-//     </div>
-//     `
+
+// console.log(rowElement)
+
+
+
+
+cardRow.innerHTML += `
+    <div class="col-3">
+        <div class="card ${classes}">
+            <h3>${result}</h3>
+        </div>
+    </div>
+    `
